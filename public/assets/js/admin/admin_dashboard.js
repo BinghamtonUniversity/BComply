62d1a23bf1744a9d053aee40374e86f96545cd60
Tim Cortesi <tcortesi@gmail.com>
2019-12-17 11:12:25 -0500

    Wrap Admin Dashboard in Panel

--- public/assets/js/admin/admin_dashboard.js
+++ public/assets/js/admin/admin_dashboard.js
@@ -1,25 +1,27 @@
 dashboard_template = `
 <div class="row">
 <duv class="col-sm-12">
+<div class="panel panel-body">
 <h1>Welcome {{first_name}} {{last_name}}</h1>
-<h3>Permissions</h3>
+<h3>My Site Permissions</h3>
 <ul>
     {{#user_permissions}}
         <li>{{.}}</li>
     {{/user_permissions}}
 </ul>
-<h3>Group Memberships</h3>
-<ul>
-    {{#pivot_groups}}
-        <li><a href="/admin/groups/{{id}}/members">{{name}}</a> ({{pivot.type}})</li>
-    {{/pivot_groups}}
-</ul>
-<h3>Module Permissions</h3>
+<h3>My Module Permissions</h3>
 <ul>
     {{#pivot_module_permissions}}
         <li><a href="/admin/modules/{{id}}/versions">{{name}}</a> ({{pivot.permission}})</li>
     {{/pivot_module_permissions}}
 </ul>
+<h3>My Group Memberships</h3>
+<ul>
+    {{#pivot_groups}}
+        <li><a href="/admin/groups/{{id}}/members">{{name}}</a> ({{pivot.type}})</li>
+    {{/pivot_groups}}
+</ul>
+</div>
 </div>
 </div>
 `;
