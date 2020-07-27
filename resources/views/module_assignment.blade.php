fe27b7d56f97994468658a0736c473539004aa41
Tim Cortesi <tcortesi@gmail.com>
2020-07-27 16:55:20 -0400

    Indicate that the configuration is corrupted.

--- resources/views/module_assignment.blade.php
+++ resources/views/module_assignment.blade.php
@@ -18,7 +18,13 @@
         @else
             <center><h3 style="text-align:center;">{{$assignment->version->name}}</h3></center>
             @if($assignment->version->type === 'articulate_tincan')
-                <iframe style="border:0px;" width="100%" height="570" src="{{url('/storage/modules/'.$assignment->module_id.'/versions/'.$assignment->module_version_id)}}/{{$assignment->version->reference->filename}}?activity_id={{$assignment->id}}&endpoint={{url('/api/tincan')}}&auth=0&actor=<?php echo htmlentities(json_encode(["name"=>Auth::user()->first_name.' '.Auth::user()->last_name,"mbox"=>Auth::user()->email]));?>"></iframe>
+                @if(!isset($assignment->version->reference->filename))
+                    <div class="alert alert-danger" style="text-align:center;align-content:center;margin:auto">
+                        <div>The tincan configuration for this module is corrupt.</div>
+                    </div>
+                @else
+                    <iframe style="border:0px;" width="100%" height="570" src="{{url('/storage/modules/'.$assignment->module_id.'/versions/'.$assignment->module_version_id)}}/{{$assignment->version->reference->filename}}?activity_id={{$assignment->id}}&endpoint={{url('/api/tincan')}}&auth=0&actor=<?php echo htmlentities(json_encode(["name"=>Auth::user()->first_name.' '.Auth::user()->last_name,"mbox"=>Auth::user()->email]));?>"></iframe>
+                @endif
             @elseif($assignment->version->type === 'youtube')
                 <div class="callout callout-info">{!! $assignment->version->reference->instructions !!}</div>
                 <div id="player"></div>
