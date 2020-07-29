b9e9f5883c1baebac1b54c782687923b8e9f4c03
Tim Cortesi <tcortesi@gmail.com>
2020-07-29 15:57:54 -0400

    Allow for null due dates

--- app/Mail/AssignmentNotification.php
+++ app/Mail/AssignmentNotification.php
@@ -22,6 +22,7 @@ class AssignmentNotification extends Mailable
      */
     public function __construct(ModuleAssignment $moduleAssignment, User $user, Array $user_message)
     {
+        $due_date = isset($moduleAssignment->date_due)?$moduleAssignment->date_due->format('m/d/y'):'N/A';
         $m = new \Mustache_Engine;
         $this->content = $m->render($user_message['assignment'],[
             'user'=>[
@@ -30,7 +31,7 @@ class AssignmentNotification extends Mailable
             ],
             'module'=>[
                 'name'=>$user_message['module_name'],
-                'due_date'=>$moduleAssignment->date_due->format('m/d/y'),
+                'due_date'=>$due_date,
                 'assignment_date'=>$moduleAssignment->date_assigned->format('m/d/y')
             ],
             'link'=>url('/assignment/'.$user_message['link'])
