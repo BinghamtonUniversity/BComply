3a62c709bfba7460a2cff8ff4f23b07e1332e574
Tim Cortesi <tcortesi@gmail.com>
2020-08-06 21:26:12 -0400

    Force Large Admin Interface on Mobile

--- resources/views/default/admin.blade.php
+++ resources/views/default/admin.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=1280">
     <meta name="description" content="">
     <meta name="author" content="">
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0" />
