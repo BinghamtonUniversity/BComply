5e564bbf406a3bfff468fd82ff6c2ff85e8da287
Tim Cortesi <tcortesi@gmail.com>
2020-12-10 16:04:41 -0500

    Hide corrupt module assignments from view

--- resources/views/assignment.blade.php
+++ resources/views/assignment.blade.php
@@ -1,5 +1,19 @@
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-    @if((\Carbon\Carbon::now()>$assignment->date_due && !$assignment->module->past_due))
+    @if(is_null($assignment->version))
+        <div class="panel panel-default past-due">
+            <a class="trigger-help" href="#" data-toggle="popover" data-placement="bottom" title="ERROR!" data-content=
+                "This assignment is corrupt.  Most likely, the module version associated with this assignment has been deleted!">
+                <div class="panel-body" style="padding-top:30px;text-align:center;">
+                    <i class="fa fa-{{$assignment->module->icon}} fa-10x"></i>
+                </div>
+                <hr style="margin:0 0px;">
+                <div class="panel-body">
+                        <div class="module-name">{{$assignment->module->name}}</div>
+                        <div class="badge">Corrupt Module</div>
+                </div>
+            </a>
+        </div>
+    @elseif((\Carbon\Carbon::now()>$assignment->date_due && !$assignment->module->past_due))
         <div class="panel panel-default past-due">
             <a class="trigger-help" href="#" data-toggle="popover" data-placement="bottom" title="Notice!" data-content=
                 "This assignment is past due, and cannot be completed after the due date!">
