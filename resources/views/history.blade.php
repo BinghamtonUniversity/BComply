8b1e9538017cb84e0bf6fdaebf9c95018c8615f1
Tim Cortesi <tcortesi@gmail.com>
2020-12-28 10:24:06 -0500

    Properly Close anchor tag

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -36,7 +36,7 @@
                     <div class="col-sm-12">
                         <div class="alert alert-warning" style="text-align:center;align-content:center;margin:auto">
                             <h4 style="margin-top:0px;">You do not have any assignments in your history!</h4>
-                            <div>Contact <a href="mailto:comply@binghamton.edu">comply@binghamton.edu if you feel that this is an error.</div>
+                            <div>Contact <a href="mailto:comply@binghamton.edu">comply@binghamton.edu</a> if you feel that this is an error.</div>
                         </div>
                     </div>
                     @endif
--- resources/views/my_assignments.blade.php
+++ resources/views/my_assignments.blade.php
@@ -18,7 +18,7 @@
         <div class="row" style="text-align: center;align-content: center;margin: auto;" >
             <div class="alert alert-warning">
                 <h4 style="margin-top:0px;">You do not have any assigned modules!</h4>
-                <div>Contact <a href="mailto:comply@binghamton.edu">comply@binghamton.edu if you feel that this is an error.</div>
+                <div>Contact <a href="mailto:comply@binghamton.edu">comply@binghamton.edu</a> if you feel that this is an error.</div>
             </div>
         </div>
     @endif
