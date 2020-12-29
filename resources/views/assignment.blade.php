00fa016da2218c3354fa7a61df8433395c31eff4
Ali Kemal Tanriverdi <11221504+alikemaltanriverdi@users.noreply.github.com>
2020-12-29 17:05:27 -0500

    added the same styling to faulty assignments
    Updated styling for the faulty assignments we missing in the last commit

--- resources/views/assignment.blade.php
+++ resources/views/assignment.blade.php
@@ -1,15 +1,21 @@
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
     @if(is_null($assignment->version))
-        <div class="panel panel-default past-due" style="min-height: 350px;">
-            <a class="trigger-help" href="#" data-toggle="popover" data-placement="bottom" title="ERROR!" data-content=
+        <div class="panel panel-default past-due" style="height: 350px;">
+            <a class="trigger-help" href="#" data-toggle="popover" data-placement="top" title="ERROR!" data-content=
                 "This assignment is corrupt.  Most likely, the module version associated with this assignment has been deleted!">
-                <div class="panel-body" style="padding-top:30px;text-align:center;">
-                    <i class="fa fa-{{$assignment->module->icon}} fa-10x"></i>
-                </div>
-                <hr style="margin:0 0px;">
-                <div class="panel-body">
-                        <div class="module-name">{{$assignment->module->name}}</div>
+                <div class="panel panel-default past-due">
+                    <div class="panel-body">
+                        <div class="panel-body" style="padding-top:30px;text-align:center;">
+                            <i class="fa fa-{{$assignment->module->icon}} fa-10x"></i>
+                        </div>
+                        <hr style="margin:0 0px;">
+                        <div class="panel-body" style="height: 110px;overflow:scroll">
+                            <div class="module-name">{{$assignment->module->name}}</div>
+                        </div>
+                    </div>
+                    <div class="panel-footer">
                         <div class="badge"><i class="fa fa-exclamation-triangle"></i> Corrupt Assignment</div>
+                    </div>
                 </div>
             </a>
         </div>
@@ -20,9 +26,6 @@
                style="height: 350px;">
                 <div class="pull-right bg-primary" style="padding: 0 5px;">{{Str::snakeToTitle($assignment->status)}}</div>
                 <div class="panel panel-default past-due" style="border: none">
-{{--                    <div class="panel-header">--}}
-{{--                        --}}
-{{--                    </div>--}}
                     <div class="panel-body">
                         <div class="panel-body" style="padding-top:30px;text-align:center;">
                             <i class="fa fa-{{$assignment->module->icon}} fa-10x"></i>
