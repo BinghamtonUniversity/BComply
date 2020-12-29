a0b0e6c0a7924e0206b686555ff97625af9f9a49
Ali Kemal Tanriverdi <11221504+alikemaltanriverdi@users.noreply.github.com>
2020-12-29 16:58:46 -0500

    Fixed styling issue in history, and added the same styling in assingments to have consistent styles in panels

--- resources/views/assignment.blade.php
+++ resources/views/assignment.blade.php
@@ -14,38 +14,57 @@
             </a>
         </div>
     @elseif((\Carbon\Carbon::now()>$assignment->date_due && !$assignment->module->past_due))
-        <div class="panel panel-default past-due" style="min-height: 350px;">
-            <a class="trigger-help" href="#" data-toggle="popover" data-placement="bottom" title="Notice!" data-content=
-                "This assignment is past due, and cannot be completed after the due date!">
+        <div class="panel panel-default past-due" style="height: 350px;">
+            <a class="trigger-help" href="#" data-toggle="popover" data-placement="top" title="Notice!" data-content=
+                "This assignment is past due, and cannot be completed after the due date!"
+               style="height: 350px;">
                 <div class="pull-right bg-primary" style="padding: 0 5px;">{{Str::snakeToTitle($assignment->status)}}</div>
-                <div class="panel-body" style="padding-top:30px;text-align:center;">
-                    <i class="fa fa-{{$assignment->module->icon}} fa-10x"></i>
-                </div>
-                <hr style="margin:0 0px;">
-                <div class="panel-body">
-                        <div class="module-name">{{$assignment->version->name}}</div>
-                        <div class="badge">Overdue: {{$assignment->date_due->format('m/d/y')}}</div>
+                <div class="panel panel-default past-due" style="border: none">
+{{--                    <div class="panel-header">--}}
+{{--                        --}}
+{{--                    </div>--}}
+                    <div class="panel-body">
+                        <div class="panel-body" style="padding-top:30px;text-align:center;">
+                            <i class="fa fa-{{$assignment->module->icon}} fa-10x"></i>
+                        </div>
+                        <hr style="margin:0 0px;">
+                        <div class="panel-body" style="height: 110px;overflow:scroll">
+                                <div class="module-name">{{$assignment->version->name}}</div>
+                        </div>
+                    </div>
+                    <div class="panel-footer" style="background-color: rgb(239 239 239);">
+                        <div class="badge" style="color: whitesmoke;background-color: #777">Overdue: {{$assignment->date_due->format('m/d/y')}}</div>
+                    </div>
                 </div>
             </a>
         </div>
     @else
-        <div class="panel panel-default" style="min-height: 350px;">
-            <a class="trigger-help" href="/assignment/{{$assignment->id}}"
-                data-toggle="popover" data-placement="bottom" title="Description" data-content="{{$assignment->module->description}}">
-                <div class="pull-right bg-primary" style="padding: 0 5px;">{{Str::snakeToTitle($assignment->status)}}</div>
-                <div class="panel-body" style="padding-top:30px;text-align:center;">
-                    <i class="fa fa-{{$assignment->module->icon}} fa-10x"></i>
-                </div>
-                <hr style="margin:0 0px;">
+
+        <a class="trigger-help" href="/assignment/{{$assignment->id}}"
+            data-toggle="popover" data-placement="top" title="Description" data-content="{{$assignment->module->description}}"
+           style="height: 350px;">
+                <div class="panel panel-default" >
+                    <div class="panel-header">
+                        <div class="pull-right bg-primary" style="padding: 0 5px;">{{Str::snakeToTitle($assignment->status)}}</div>
+                    </div>
                 <div class="panel-body">
-                        <div class="module-name">{{$assignment->version->name}}</div>
-                        @if(isset($assignment->date_due))
-                            <div class="badge">Due: {{$assignment->date_due->format('m/d/y')}}</div>
-                        @else 
-                            <div class="badge">No Due Date</div>
-                        @endif
+                    <div class="panel-body" style="padding-top:30px;text-align:center;">
+                        <i class="fa fa-{{$assignment->module->icon}} fa-10x"></i>
+                    </div>
+                    <hr style="margin:0 0px;">
+                    <div class="panel-body" style="height: 110px;overflow:scroll">
+                            <div class="module-name">{{$assignment->version->name}}</div>
+                    </div>
                 </div>
-            </a>
-        </div>
+                <div class="panel-footer">
+                    @if(isset($assignment->date_due))
+                        <div class="badge">Due: {{$assignment->date_due->format('m/d/y')}}</div>
+                    @else
+                        <div class="badge">No Due Date</div>
+                    @endif
+                </div>
+            </div>
+        </a>
+
     @endif
 </div>
