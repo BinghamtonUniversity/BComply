508ea07216ad6f925e898a72e625065506a0686c
Tim Cortesi <tcortesi@gmail.com>
2021-06-09 14:09:01 -0400

    Remove chown / chgrp from deployment script

--- .platform/hooks/postdeploy/99_create_public_symlink.sh
+++ .platform/hooks/postdeploy/99_create_public_symlink.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 FILE_STORAGE_PATH=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.FILE_STORAGE_PATH')
 mkdir -p $FILE_STORAGE_PATH/public/modules
-chown -R webapp /efs
-chgrp -R webapp /efs
 ln -sf $FILE_STORAGE_PATH/public /var/app/current/public/storage
