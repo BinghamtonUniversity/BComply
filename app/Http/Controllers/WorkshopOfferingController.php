132f2732e45ccea896c9dc4b61320dd737e4868e
Ali Kemal Tanriverdi <11221504+alikemaltanriverdi@users.noreply.github.com>
2022-03-17 08:37:12 -0400

    added models, controllers, migrations for new requirements

---
+++ app/Http/Controllers/WorkshopOfferingController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WorkshopOfferingController extends Controller
+{
+    //
+}
---
+++ database/migrations/2022_03_17_083441_create_workshops_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkshopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workshops', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workshops');
+    }
+}
