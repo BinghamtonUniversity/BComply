9af199460494fbb5a5db237ae9794e5251cb40db
cankaturkmen <cturkmen@binghamton.edu>
2022-04-01 11:11:52 -0400

    403 Forbidden & cancel registration modal & status=>attandence & seats reamining & if date is past

--- resources/views/workshop.blade.php
+++ resources/views/workshop.blade.php
@@ -1,28 +1,37 @@
 <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
     {{-- <a class="trigger-help"    href="/assignment/{{$assignment->id}}" --}}
-        <a class="trigger-help"    href=""
-            data-toggle="popover" data-placement="top" title="Description" data-content="{{$workshop->description}}"
-           style="height: 350px;">
+        @if($attendance->workshop->public)
+        <a class="trigger-help" href="#"
+        data-toggle="popover" data-placement="top" title="" data-content="Public"
+    style="height: 350px">
+    @else
+        <a class="trigger-help" href="#"
+        data-toggle="popover" data-placement="top" title="" data-content="Private"
+    style="height: 350px">
+    @endif
                 <div class="panel panel-default" >
                     <div class="panel-header">
-                        <div class="pull-right bg-primary" style="padding: 0 5px;">{{Str::snakeToTitle($workshop->public)}}</div>
+                        <div class="pull-right bg-primary" style="padding: 0 5px;">{{Str::snakeToTitle($attendance->status)}}</div>
                     </div>
                 <div class="panel-body">
                     <div class="panel-body" style="padding-top:30px;text-align:center;">
-                        <i class="fa fa-{{$workshop->icon}} fa-10x"></i>
+                        <i class="fa fa-{{$attendance->workshop->icon}} fa-10x"></i>
                     </div>
                     <hr style="margin:0 0px;">
                     <div class="panel-body" style="height: 110px;overflow:scroll">
-                            <div class="module-name">{{$workshop->name}}</div>
+                            <div class="module-name">{{$attendance->workshop->name}}</div>
+                            <div class="workshop-description">{{$attendance->workshop->description}}</div>
                     </div>
                 </div>
-                {{-- <div class="panel-footer">
-                    @if(isset($assignment->date_due))
-                        <div class="badge">Due: {{$assignment->date_due->format('m/d/y')}}</div>
-                    @else
-                        <div class="badge">No Due Date</div>
-                    @endif
-                </div> --}}
+                <div class="panel-footer">
+                    <ul>
+                        <li><div class="badge">Location: {{$attendance->workshop_offering->locations}}</div></li>
+                        <li><div class="badge">Type: {{$attendance->workshop_offering->type}}</div></li>
+                        <li><div class="badge">Instructor: {{$attendance->workshop_offering->instructor->first_name}} {{$attendance->workshop_offering->instructor->last_name}}</div></li>
+                        <li><div class="badge">Date: {{$attendance->workshop_offering->workshop_date}}</div></li>
+                    </ul>
+                
+                </div>
             </div>
         </a>
 
