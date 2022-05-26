814c58bd0b53d11cc773fc6c2804559b149a9258
Tim Cortesi <tcortesi@gmail.com>
2022-05-26 15:39:56 -0400

    Rollback Extra .env Config

--- .platform/confighooks/postdeploy/99_create_public_symlink.sh
+++ .platform/confighooks/postdeploy/99_create_public_symlink.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-# Target .env file location: 
-DOTENV_FILE="/var/app/current/.env"
-
-# Extract the Elastic Beanstalk Environment Configuration Data
-ENVIRONMENT_JSON=`/opt/elasticbeanstalk/bin/get-config environment`
-
-# Clear out any existing .env file data
-cat /dev/null > $DOTENV_FILE
-
-# Convert the ENVIRONMENT_JSON to .env file format
-while read -rd $'' line
-do
-    echo "$line" >> $DOTENV_FILE
-done < <(jq -r <<< $ENVIRONMENT_JSON 'to_entries|map("\(.key)=\"\(.value)\"\u0000")[]')
-
 FILE_STORAGE_PATH=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.FILE_STORAGE_PATH')
 mkdir -p $FILE_STORAGE_PATH/public/modules
 mkdir -p $FILE_STORAGE_PATH/public/workshops
