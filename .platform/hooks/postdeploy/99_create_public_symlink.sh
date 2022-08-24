543f0590d913bcba8475de4a7f143c40a04fddf2
Tim Cortesi <tcortesi@gmail.com>
2022-08-24 14:49:27 -0400

    revert

--- .platform/hooks/postdeploy/99_create_public_symlink.sh
+++ .platform/hooks/postdeploy/99_create_public_symlink.sh
@@ -1,5 +1,5 @@
-
 #!/usr/bin/env bash
+
 FILE_STORAGE_PATH=$(/opt/elasticbeanstalk/bin/get-config environment | jq -r '.FILE_STORAGE_PATH')
 mkdir -p $FILE_STORAGE_PATH/public/modules
 mkdir -p $FILE_STORAGE_PATH/public/workshops
