294b952f0266baa2e5ea5918c9019b148eda682a
Tim Cortesi <tcortesi@gmail.com>
2024-06-18 13:21:18 -0400

    Add registration and id for xapi

--- app/Http/Controllers/TinCanController.php
+++ app/Http/Controllers/TinCanController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Builder;
 use App\ModuleAssignment;
 
 class TinCanController extends Controller
-{
+{   
     public function get_state(Request $request) {
-        $assignment = ModuleAssignment::where('user_id',Auth::user()->id)
-            ->where('id',$request->activityId)->first();
+        $assignment = ModuleAssignment::where('user_id',Auth::user()->id)->where(function (Builder $query) {
+            $query->orWhere('id',$request->id)
+                ->where('id',$request->activityId)
+                ->orWhere('id',$request->registration);
+        })->first();
         if (!is_null($assignment)) {
             if (isset($assignment->current_state->state)) {
                 return $assignment->current_state->state;
@@ -21,8 +25,11 @@ class TinCanController extends Controller
         return response('Assignment Does Not Exist',404);
     }
     public function set_state(Request $request) {
-        $assignment = ModuleAssignment::where('user_id',Auth::user()->id)
-            ->where('id',$request->activityId)->first();
+        $assignment = ModuleAssignment::where('user_id',Auth::user()->id)->where(function (Builder $query) {
+            $query->orWhere('id',$request->id)
+                ->where('id',$request->activityId)
+                ->orWhere('id',$request->registration);
+        })->first();
         if (!is_null($assignment)) {
             $assignment->current_state = (Object)[
                 'state' => $request->getContent()
