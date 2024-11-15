f529dbc058da1d1edd5612564a3cc9103c16b4da
Tim Cortesi <tcortesi@gmail.com>
2024-11-14 19:46:21 -0500

    Fix Automated Assignment Reminder Emails

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -41,12 +41,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-
         //Workshop Reminder Scheduler
         $schedule->call(function(){
             $workshopAttendances = WorkshopAttendance::where('attendance','registered')->with('attendee')->with('workshop_offering')->with('workshop')->get();   
             foreach($workshopAttendances as $attendance){
-                try{
+                try {
                     $differenceInDays = $attendance->workshop_offering->workshop_date->diffInDays(Carbon::now());
                     if($differenceInDays == 1){
                         $user = $attendance->attendee;
@@ -56,18 +55,16 @@ class Kernel extends ConsoleKernel
                                 'offering_date' =>$attendance->workshop_offering->workshop_date,
                                 'reminder'=> $attendance->workshop->config->reminder
                             ];
-                            Mail::to($user)->send(new WorkshopReminder($attendance, $user, $user_message));
+                            Log::info('Reminder Email to: '.$user->email.' for workshop '.$attendance->workshop->name);
+                            Mail::to($user->email)->send(new WorkshopReminder($attendance, $user, $user_message));
                         }
                     }
-                }
-                catch(\Exception $exception){
-                    //Keep going
+                } catch(\Exception $exception){
+                    Log::error('Error sending reminder email: '.$exception->getMessage());
                 }
             }
 
         })->name('workshop_reminder_task')->dailyAt(config('app.workshop_reminder_task'))->timezone('America/New_York')->onOneServer();
-
-
 
         // Assignment Reminder Scheduler
         $schedule->call(function(){
@@ -76,13 +73,19 @@ class Kernel extends ConsoleKernel
 
             //Checks all of the assignment due dates to send emails to users
             foreach($module_assignments as $assignment){
-                try{
+                try {
                     $module = $modules->where('id', $assignment->module_id)->first();
-                    $user = $assignment->user()->where('id', $assignment->user_id)->first();
+                    $user = $assignment->user_full()->where('id', $assignment->user_id)->first();
                     if ($user->active && $user->send_email_check()) {
                         if (isset($assignment->date_due) && !is_null($assignment->date_due)) {
                             $differenceInDays = $assignment->date_due->diffInDays(Carbon::now());
                             $user_message = null;
+                            if (!is_array($module->past_due_reminders)) {
+                                $module->past_due_reminders = [];
+                            }
+                            if (!is_array($module->reminders)) {
+                                $module->reminders = [];
+                            }
                             if ($assignment->date_due <= Carbon::now() && $module->past_due && in_array($differenceInDays, $module->past_due_reminders) && $module->templates->past_due_reminder != '') {
                                 $user_message = [
                                     'module_name' => $module['name'],
@@ -97,12 +100,13 @@ class Kernel extends ConsoleKernel
                                 ];
                             }
                             if (!is_null($user_message)) {
-                                Mail::to($user)->send(new AssignmentReminder($assignment, $user, $user_message));
+                                Log::info('Reminder Email to: '.$user->email.' for module '.$module['name']);
+                                Mail::to($user->email)->send(new AssignmentReminder($assignment, $user, $user_message));
                             }
                         }
                     }
-                }catch(\Exception $exception){
-                    //Keep going
+                } catch(\Exception $exception){
+                    Log::error('Error sending reminder email: '.$exception->getMessage());
                 }
             }
         })->name('assignment_reminder_task')->dailyAt(config('app.assignment_reminder_task'))->timezone('America/New_York')->onOneServer();
@@ -112,7 +116,7 @@ class Kernel extends ConsoleKernel
             $bulkAssignments = BulkAssignment::whereJsonContains('assignment',['later_date'=>true])->get();
             if(!is_null($bulkAssignments)){
                 foreach($bulkAssignments as $bulkAssignment){
-                    try{
+                    try {
                         if (Carbon::parse($bulkAssignment->assignment->later_assignment_date)->isToday()) {
                             $module = Module::where('id', $bulkAssignment->assignment->module_id)->with('current_version')->first();
                             if (is_null($module) || is_null($module->module_version_id)) {
@@ -129,6 +133,7 @@ class Kernel extends ConsoleKernel
                                 } else {
                                     $date_due = $bulkAssignment->assignment->date_due;
                                 }
+                                Log::info('Automated Assignment to: '.$user->email.' for '.$module['name']);
                                 $module->assign_to([
                                     'user_id' => $user->id,
                                     'date_due' => $date_due,
@@ -136,8 +141,8 @@ class Kernel extends ConsoleKernel
                                 ]);
                             }
                         }
-                    }catch(\Exception $e){
-                        //Keep going
+                    } catch(\Exception $e){
+                        Log::error('Error assigning module: '.$exception->getMessage());
                     }
                 }
             }
@@ -152,7 +157,6 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
     }
 }
