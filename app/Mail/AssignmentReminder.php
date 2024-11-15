fc49f62899512f8f74e186183f7d2a36186256f8
Tim Cortesi <tcortesi@gmail.com>
2024-11-15 08:44:51 -0500

    Dynamic assignment_date_plus fields

--- app/Mail/AssignmentReminder.php
+++ app/Mail/AssignmentReminder.php
@@ -25,8 +25,7 @@ class AssignmentReminder extends Mailable
     public function __construct(ModuleAssignment $moduleAssignment, SimpleUser $user, Array $user_message)
     {
         $due_date = isset($moduleAssignment->date_due)?$moduleAssignment->date_due->format('m/d/y'):'N/A';
-        $m = new \Mustache_Engine;
-        $this->content = $m->render($user_message['reminder'],[
+        $email_data = [
             'user'=>[
                 'first_name'=> $user->first_name,
                 'last_name'=>$user->last_name,
@@ -37,14 +36,15 @@ class AssignmentReminder extends Mailable
                 'assignment_date'=>$moduleAssignment->date_assigned->format('m/d/y')
             ],
             'link'=>url('/assignment/'.$user_message['link'])
-        ]);
+        ];
+        for($days=1;$days<=60;$days++) {
+            $email_data['module']['assignment_date_plus_'.$days]= floor($moduleAssignment->date_assigned->addDays($days));
+        }
+        $m = new \Mustache_Engine;
+        $this->content = $m->render($user_message['reminder'],$email_data);
         $this->moduleAssignment = $moduleAssignment;
         $this->user = $user;
         $this->user_message = $user_message;
-//
-//        $this->moduleAssignment = $moduleAssignment;
-//        $this->user = $user;
-//        $this->user_message = $user_message;
     }
 
     /**
--- app/Mail/CompletionNotification.php
+++ app/Mail/CompletionNotification.php
@@ -23,20 +23,23 @@ class CompletionNotification extends Mailable
     public function __construct(ModuleAssignment $moduleAssignment, User $user, Array $user_message)
     {
         $due_date = isset($moduleAssignment->date_due)?$moduleAssignment->date_due->format('m/d/y'):'N/A';
+        $email_data = [
+            'user'=>[
+                'first_name'=> $user->first_name,
+                'last_name'=>$user->last_name,
+            ],
+            'module'=>[
+                'name'=>$user_message['module_name'],
+                'due_date'=>$due_date,
+                'assignment_date'=>$moduleAssignment->date_assigned->format('m/d/y')
+            ],
+            'link'=>url('/assignment/'.$user_message['link'])
+        ];
+        for($days=1;$days<=60;$days++) {
+            $email_data['module']['assignment_date_plus_'.$days]= floor($moduleAssignment->date_assigned->addDays($days));
+        }
         $m = new \Mustache_Engine;
-        $this->content = $m->render($user_message['completion_notification'],
-            [
-                'user'=>[
-                    'first_name'=> $user->first_name,
-                    'last_name'=>$user->last_name,
-                    ],
-                'module'=>[
-                    'name'=>$user_message['module_name'],
-                    'due_date'=>$due_date,
-                    'assignment_date'=>$moduleAssignment->date_assigned->format('m/d/y')
-                ],
-                'link'=>url('/assignment/'.$user_message['link'].'/certificate')
-        ]);
+        $this->content = $m->render($user_message['completion_notification'],$email_data);
         $this->moduleAssignment = $moduleAssignment;
         $this->user = $user;
         $this->user_message = $user_message;
