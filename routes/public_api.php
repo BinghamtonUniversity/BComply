37699c39cc325f6e8f78b73e29b3bebcd2d13b18
Chris McLain <chris@humansarehuman.com>
2025-07-10 16:12:00 -0400

    Added test urls

--- routes/public_api.php
+++ routes/public_api.php
@@ -22,6 +22,8 @@ Route::put('/users/{unique_id}','PublicAPIController@update_user');
 /**
  * Get all the assignments that fit a user
  *  return rows from the assignments table
+ * 
+ * test - http://bcomplydev.local:8000/api/public/users/B00168387/assignments
  */
 Route::get('/users/{unique_id}/assignments','PublicAPIController@get_user_assignments');
 
@@ -34,11 +36,15 @@ Route::get('/users/{unique_id}/assignments','PublicAPIController@get_user_assign
  *      
  *  returns:
  *      rows from modules table
+ * 
+ *  test - http://bcomplydev.local:8000/api/public/modules?module_name=%test%
  */
 Route::get('/modules','PublicAPIController@get_modules_by_name');
 
 /**
  *  lookup all the users that have a module assigned
+ * 
+ *  test - http://bcomplydev.local:8000/api/public/modules/2/assignments
  */
 Route::get('/modules/{module}/assignments','PublicAPIController@get_module_assignments');
 
@@ -50,6 +56,8 @@ Route::get('/modules/{module}/assignments','PublicAPIController@get_module_assig
  *      status (optional) - only return records that have the status specified
  *  returns:
  *      all rows from module_assignments for student and assignment
+ * 
+ *  test - http://bcomplydev.local:8000/api/public/modules/2/users/B00168387
  */
 Route::get('/modules/{module}/users/{unique_id}','PublicAPIController@get_user_module_status');
 
@@ -57,6 +65,8 @@ Route::get('/modules/{module}/users/{unique_id}','PublicAPIController@get_user_m
  * Assigns a module to a user
  *  parameters:
  *      due_date (optional) - (formated as 2025-04-29) - null if omitted
+ * 
+ * test - http://bcomplydev.local:8000/api/public/modules/2/users/B00168387?due_date=2025-08-15
  */
 Route::post('/modules/{module}/users/{unique_id}', 'PublicAPIController@assign_module_to_user');
 
@@ -68,16 +78,22 @@ Route::post('/modules/{module}/users/{unique_id}', 'PublicAPIController@assign_m
  *                               if omitted return all groups
  *  returns:
  *      rows from groups table
+ * 
+ *  test - http://bcomplydev.local:8000/api/public/groups?group_name=%test%
  */
 Route::get('/groups', 'PublicAPIController@get_groups_by_name');
 
 /**
  * adds a user to a group (or updates the user if they already exist in the group)
+ * 
+ *  test - http://bcomplydev.local:8000/api/public/groups/api_group1/users/B00168387
  */
 Route::post('/groups/{slug}/users/{unique_id}','PublicAPIController@add_group_membership');
 
 /**
  * removes a user from a group
+ * 
+ *  test - http://bcomplydev.local:8000/api/public/groups/api_group1/users/B00168387
  */
 Route::delete('/groups/{slug}/users/{unique_id}','PublicAPIController@delete_group_membership');
 
@@ -89,6 +105,8 @@ Route::delete('/groups/{slug}/users/{unique_id}','PublicAPIController@delete_gro
  *      version (optional) 
  *  returns: 
  *      all rows for module_assigments that fit the criteria plus the module_name, group_name, user_name, and boolean for completed
+ * 
+ *  test - http://bcomplydev.local:8000/api/public/groups/test_group/modules/2?assigned_after=2025-01-01
  */
 Route::get('/groups/{slug}/modules/{module}','PublicAPIController@get_group_module_status');
 
@@ -100,6 +118,8 @@ Route::get('/groups/{slug}/modules/{module}','PublicAPIController@get_group_modu
  *      due_date (optional): enter a due date for the assignment specified like 2025-05-01
  *  returns:
  *      the module that was assigned or an error message
+ * 
+ *  test - http://bcomplydev.local:8000/api/public/groups/test_group/modules/2?due_date=2026-01-01
  */
 
 Route::post('/groups/{slug}/modules/{module}','BulkAssignmentController@assign_module_to_group_members'); 
@@ -110,6 +130,8 @@ Route::post('/groups/{slug}/modules/{module}','BulkAssignmentController@assign_m
  *      none
  *  returns:
  *      the users in the group
+ * 
+ *  - http://bcomplydev.local:8000/api/public/groups/1
  */
 Route::get('/groups/{group}','PublicAPIController@get_all_group_users'); 
 
@@ -119,20 +141,11 @@ Route::get('/groups/{group}','PublicAPIController@get_all_group_users');
  *      group_name (optional): if not given, the group name is the same as the slug
  *  returns:
  *      the group row or a message why the group could not be created
+ * 
+ *  test - http://bcomplydev.local:8000/api/public/groups/api_group5?group_name=New%20group5
  */
 Route::post('/groups/{slug}','PublicAPIController@create_group'); 
 
-//Bulk Assignment
-/**
- * Create a bulk assignment
- *  parameters:
- *      assignment: json of the assignment
- *      description (optional): assignment description, defaults to ''
- *  returns:
- *      the assignment row that was created or an error message
- */
-
- Route::post('/bulkAssignments/{assignmentName}', 'BulkAssignments@create_bulk_assignments');
 
 //Workshops
 /**
@@ -144,51 +157,11 @@ Route::post('/groups/{slug}','PublicAPIController@create_group');
  *      rows from workshops table
  */
 
-// Route::get('/workshops','PublicAPIController@get_workshops_by_name');
-
-/**
- *  gets all workshop attendance for the users of a group where the workshop id = workshop_id
- *  parameters:
- *      after (optional) - only return records where the workshop date is after a specifice date (formated as 2025-04-29)
- *  returns: 
- *      workshop, workshop_attendance, workshop_offering, group, and user data
- */
-Route::get('/groups/{slug}/workshops/{workshop_id}','PublicAPIController@get_group_users_status_for_workshops');
-
-/**
- * Add workshop_attendance record for everyone in a group
- *  parameters:
- *      status (optional) - "not_applicable", "uncompleted", "completed" defaults to uncomplete
- *      attendance (optional) - "registered", "attended", "completed" defaults to registered
- */
-
-Route::post('/groups/{slug}/workshops/{workshop_id}', 'BulkAssignment@add_workshop_attendence_to_group_members');
-
-/**
- * Gets the workshop_attendance record for a user
- *  parameters:
- *      after(optional) - only return attendance records after the specified date (formated as 2025-04-29)
- */
-Route::get('/workshops/{workshop_id}/users/{unique_id}','PublicAPIController@get_workshop_attendance_for_user');
-
-/**
- * set the status and attendance for a workshop and user
- *  parameters:
- *      status (required) - "not_applicable", "uncompleted", "completed"
- *      attendance (required) - "registered", "attended", "completed"
- */
-Route::put('/workshops/{workshop_id}/users/{unique_id}', 'PublicAPIController@update_user_workshop_status_and attendance');
-
-
-/**
- * add the user to the workshop attendance table
- *  parameters:
- *      status (optional) - "not_applicable", "uncompleted", "completed" defaults to uncomplete
- *      attendance (optional) - "registered", "attended", "completed" defaults to registered
- */
-Route::post('/workshops/{workshop_id}/users/{unique_id}', 'PublicAPIController@add_workshop_attendance_for_user');
+ Route::get('/workshops','PublicAPIController@get_workshops_by_name');
 
 
 //todo New Impersonate
 //Use the same pass_reset methods
 Route::get('/users/{unique_id}/impersonate', 'PublicAPIController@impersonate_user');
+
+
