ab81b8da7299bd6409c13e89665c031ef02b1840
Ali Kemal Tanriverdi <11221504+alikemaltanriverdi@users.noreply.github.com>
2025-10-17 12:13:18 -0400

    Revert "added update_assignment_status"
    This reverts commit 9a246f10a0f7e413ca5675209470a4d7bc0cd27d.

--- routes/public_api.php
+++ routes/public_api.php
@@ -67,13 +67,6 @@ Route::get('/modules/{module}/assignments','PublicAPIController@get_module_assig
  */
 Route::get('/modules/{module}/assignments_data','PublicAPIController@get_module_assignments_data');
 
-/**
- *   Given a module and a user, update the assignment status. If the assignment does not exist,
- *   parameters:
- *      status (required) - the new status
- */
-
-Route::put('/modules/{module}/users/{unique_id}', 'PublicAPIController@update_assignment_status');
 
 /**
  * Assigns a module to a user
@@ -83,7 +76,6 @@ Route::put('/modules/{module}/users/{unique_id}', 'PublicAPIController@update_as
  * test - http://bcomplydev.local:8000/api/public/modules/2/users/B00168387?due_date=2025-08-15
  */
 Route::post('/modules/{module}/users/{unique_id}', 'PublicAPIController@assign_module_to_user');
-
 
 // Groups
 /**
